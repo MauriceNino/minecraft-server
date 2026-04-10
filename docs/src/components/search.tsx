218776ad85532ab10095cb017846aed8a7457db6
MauriceNino <mauriceprivat98@gmail.com>
2026-04-10 16:23:17 +0200

    docs: restore functionality for search bar

--- docs/src/components/search.tsx
+++ docs/src/components/search.tsx
@@ -23,9 +23,10 @@ function initOrama() {
 }
 
 export default function DefaultSearchDialog(props: SharedProps) {
-  const { locale } = useI18n(); // (optional) for i18n
+  const { locale } = useI18n();
   const { search, setSearch, query } = useDocsSearch({
     type: 'static',
+    from: `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/api/search`,
     initOrama,
     locale,
   });
